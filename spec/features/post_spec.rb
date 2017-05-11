a00750c0f4dd126d4ac14c8cea9d60fe6b86c6e5
Dave <davidlarsen39@gmail.com>
2017-05-11 09:43:01 -0600

    updated app and schema

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,19 +2,26 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
-    it 'can be reached successfully' do
+    before do 
+      user = User.create(email: "gaga@gaga.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow")
+      login_as(user, :scope => :user)
       visit posts_path
-      expect(page.status_code).to eq(200)
     end
+      it 'can be reached successfully' do
+          visit posts_path
+          expect(page.status_code).to eq(200)
+      end
 
-    it 'has a title of Posts' do
-      visit posts_path
-      expect(page).to have_content(/Posts/)
-    end
+      it 'has a title of Posts' do
+          visit posts_path
+          expect(page).to have_content(/Posts/)
+      end
   end
 
   describe 'creation' do
     before do
+      user = User.create(email: "gaga@gaga.com", password: "123456", password_confirmation: "123456", first_name: "Gaga", last_name: "Gaga")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -28,6 +35,14 @@ describe 'navigate' do
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe "Creation" do
-  	before do
-  		@post = Post.create(date: Date.today, rationale: "Anything")
-  	end
+    before do
+      user = User.create(email: "gaga@gaga.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow") #added a user
+      login_as(user, :scope => :user) #logged in as that user
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: user.id) #set user_id for the post
+    end
 
   	it 'can be created' do	
   		expect(@post).to be_valid

7aea5349b4fe3b19000e393ee7536bf49c345008
Dave <davidlarsen39@gmail.com>
2017-05-15 15:32:25 -0600

    implemented seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
-@user = User.create(email: "gaga@gaga.com", password: "123456", password_confirmation: "123456", first_name: "gaga", last_name: "gaga")
+
+@user = User.create(email: "admin@test", password: "123456", password_confirmation: "123456", first_name: "Admin", last_name: "Name")
 
 puts "1 User created"
+
+AdminUser.create(email: "admin@test.com", password: "123456", password_confirmation: "123456", first_name: "Admin", last_name: "Name")
+
+puts "1 Admin User created"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)

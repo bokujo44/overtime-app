15b671c07dae6d99c39f9e6071690d07e7b740e5
Dave <davidlarsen39@gmail.com>
2017-05-23 14:30:34 -0600

    Created resource for audit log

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516173703) do
+ActiveRecord::Schema.define(version: 20170523202216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "audit_logs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "status",     default: 0
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["user_id"], name: "index_audit_logs_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
@@ -23,9 +33,8 @@ ActiveRecord::Schema.define(version: 20170516173703) do
     t.integer  "user_id"
     t.integer  "status",           default: 0
     t.decimal  "overtime_request", default: "0.0"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
-
-  t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -49,5 +58,6 @@ ActiveRecord::Schema.define(version: 20170516173703) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "audit_logs", "users"
   add_foreign_key "posts", "users"
 end
